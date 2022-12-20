9c2fdcc4908c55854ef604a8c41cfc54c500e461
tribber93 <tribberyoni5@gmail.com>
2022-12-20 14:05:18 +0700

    koneksi ke firebase

--- lib/app/controller/myController.dart
+++ lib/app/controller/myController.dart
@@ -1,16 +1,28 @@
+import 'dart:math';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 
 class MyController extends GetxController {
   //TODO: Implement DetailPaketController
+  var db = FirebaseFirestore.instance;
+  var tanggal = DateFormat("EEEE, d MMM yyyy", "id_ID").format(DateTime.now());
 
   bool isChecked = false;
   var tabIndex = 0;
   var count = 1.obs;
   RxInt total = 0.obs;
+  DateTime tanggalSekarang = DateTime.now();
+
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  late TextEditingController tanggalController;
 
   @override
   void onInit() {
     super.onInit();
+    tanggalController = TextEditingController();
   }
 
   @override
@@ -21,6 +33,7 @@ class MyController extends GetxController {
   @override
   void onClose() {
     super.onClose();
+    tanggalController.dispose();
   }
 
   void increment() {
@@ -38,6 +51,11 @@ class MyController extends GetxController {
     update();
   }
 
+  void radioButton(val, i, selected, info) {
+    debugPrint('Button: $val index: $i $selected');
+    debugPrint(info.harga.toString());
+  }
+
   void addTotal(info, int index) {
     int harga = info[index]["harga"];
     if (info[index]["isCheck"] == true) {
@@ -46,4 +64,82 @@ class MyController extends GetxController {
       total = total - harga;
     }
   }
+
+  addUser() async {
+    // var userId = DateTime.now().toIso8601String();
+    final user = <String, dynamic>{
+      "first": "Isaura",
+      "last": "Anindia",
+      "born": 2011
+    };
+    await db
+        .collection("users")
+        .add(user)
+        .whenComplete(() => Get.snackbar("Berhasil", "Pesan Berhasil dikirim"));
+  }
+
+  Future<void> addJadwal({
+    String? docId,
+    Timestamp? ts,
+    String? hari,
+    String? tgl,
+  }) async {
+    if (await checkIfDocExists(docId)) {
+      Get.snackbar(
+        "Generate Gagal",
+        "Jadwal sudah ada",
+        backgroundColor: Colors.red,
+      );
+      return;
+    }
+
+    db.collection("jadwal").doc(docId).set({
+      "timeStamp": ts,
+      "hari": hari,
+      "tanggal": tgl,
+      "jadwal": jadwal,
+    }).whenComplete(() => Get.snackbar("Berhasil", "Jadwal sudah digenerate",
+        backgroundColor: Colors.green));
+  }
+
+  Stream<QuerySnapshot> streamJadwal() {
+    return db
+        .collection('jadwal')
+        .where(
+          'timeStamp',
+          isGreaterThanOrEqualTo: Timestamp.fromDate(DateTime(
+              tanggalSekarang.year,
+              tanggalSekarang.month,
+              tanggalSekarang.day)),
+        )
+        .snapshots();
+  }
+
+  Future<bool> checkIfDocExists(String? docId) async {
+    try {
+      // Get reference to Firestore collection
+      var collectionRef = db.collection('jadwal');
+
+      var doc = await collectionRef.doc(docId).get();
+      // debugPrint("id --> ${doc}" + doc.exists.toString());
+      return doc.exists;
+    } catch (e) {
+      rethrow;
+    }
+  }
 }
+
+List jadwal = [
+  {"jam": '10:00', "isBooked": false},
+  {"jam": '10:30', "isBooked": false},
+  {"jam": '11:00', "isBooked": false},
+  {"jam": '11:30', "isBooked": false},
+  {"jam": '12:00', "isBooked": false},
+  {"jam": '12:30', "isBooked": false},
+  {"jam": '13:00', "isBooked": false},
+  {"jam": '13:30', "isBooked": false},
+  {"jam": '14:00', "isBooked": false},
+  {"jam": '14:30', "isBooked": false},
+  {"jam": '15:00', "isBooked": false},
+  {"jam": '15:30', "isBooked": false},
+];
